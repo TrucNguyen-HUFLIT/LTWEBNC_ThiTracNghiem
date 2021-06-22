5753bf7924ac1f24c4243fa8a7ff1ba8f1740ed2
ngoctruc0201 <ngoctruc020100@gmail.com>
2021-06-22 23:30:38 +0700

    hoàn thành thêm bài thi

---
+++ DA_WEBNC/DA_WEBNC/Models/AddCHViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DA_WEBNC.Models
+{
+    public class AddCHViewModel
+    {
+        public BaiThi BaiThi { get; set; }
+        public List<CauHoi> ListCauHoi { get; set; }
+    }
+}
